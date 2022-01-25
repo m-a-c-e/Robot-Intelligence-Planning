538955b7bf278d6607d43a942cca9d1afa394500
Mace <patelmanan1998@gmail.com>
2022-01-25 11:32:13 -0500

    python file added

---
+++ PS03/pset3.py
@@ -0,0 +1,27 @@
+class PSet3():
+
+	def standard(self):
+		pass
+		# return a list of the time taken for each value of n
+		# ex: return [1,2,3,5,10]
+
+	def BT(self):
+		pass
+		# return a list of the time taken for each value of n
+		# ex: return [1,2,3,5,10]
+
+
+	def BT_w_FC(self):
+		pass
+		# return a list of the time taken for each value of n
+		# ex: return [1,2,3,5,10]
+
+
+	def iterative_repair(self):
+		pass
+		# return a list of the time taken for each value of n
+		# ex: return [1,2,3,5,10]
+
+		
+
+
